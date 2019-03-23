f9621e28c4187ec2732a5977df1c78633fbc1a74
Maurício R. Duarte <mauriciotem@gmail.com>
2019-03-23 15:07:06 -0300

    refactor: another test

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,6 @@ const instructions = Platform.select({
 const App: SFC<{}> = props => (
   <View style={styles.container}>
     <Text style={styles.welcome}>Welcome to React Native TS!</Text>
-    <Text style={styles.welcome}>Tst 1!</Text>
-    <Text style={styles.welcome}>Test 2!</Text>
     <Text style={styles.instructions}>To get started, edit App.tsx</Text>
     <Text style={styles.instructions}>{instructions}</Text>
   </View>
